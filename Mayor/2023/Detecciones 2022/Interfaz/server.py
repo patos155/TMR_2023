4ac3bb91ace7239c8c1a21d7034304f722c6fd66
Fntr <119082688+Fntr9630859@users.noreply.github.com>
2023-09-09 20:24:43 -0600

    Interfaz
    La interfaz por el momento solamente detecta codigos qr con opencv y muestra los resultados en tkinter

---
+++ Mayor/2023/Detecciones 2022/Interfaz/server.py
@@ -0,0 +1,61 @@
+from flask import *
+import cv2
+import numpy as np
+from tkinter import *
+
+
+cap = cv2.VideoCapture(0)
+detector = cv2.QRCodeDetector()
+
+datos = ""
+
+def mensaje():
+    global datos,data
+
+    if not(datos == data):
+        carcasa = Tk()
+        carcasa.title("Mensaje:")
+        carcasa.geometry("250x100")
+        Label(carcasa, text=data).pack(expand=1)
+        carcasa.mainloop()
+    datos = data
+
+def generar():
+    global data
+    while True:
+        _, frame = cap.read()
+        # get bounding box coords and data
+        data, bbox, _ = detector.detectAndDecode(frame)
+    
+        # if there is a bounding box, draw one, along with the data
+        if(bbox is not None):
+            bb_pts = bbox.astype(int).reshape(-1, 2)
+            for i in range(len(bb_pts)):
+                cv2.line(frame, tuple(bb_pts[i]), tuple(bb_pts[(i+1) % len(bb_pts)]), color=(255,
+                         0, 255), thickness=2)
+            cv2.putText(frame, data, (bb_pts[0][0], bb_pts[0][1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.5, (0, 255, 0), 2)
+            if data:
+                print(data)
+                mensaje()
+        (flag, encodeimage) = cv2.imencode(".jpg",frame)
+        if not flag:
+            continue
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encodeimage) + b'\r\n') 
+        
+        
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('interfaz.html')
+
+@app.route('/video')
+def video():
+    return Response(generar(),
+                    mimetype="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    app.run(debug=False)
